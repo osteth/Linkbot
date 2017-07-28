eacfcc71c894546bf772fc4a78507fdfe90f31ec
osteth <osteth@gmail.com>
2017-07-27 22:33:55 -0500

    initial release of bot

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Linkbot
-Linkeding Connection Bot using python and selenium
+
+Automatically sends connectino requests to everyone on your linkedin my network page. 
+
+## Getting Started 
+This bot requires python3+ and selenium to work 
+```
+pip install selenium
+```
+Next, Chromedriver and Geckkodriver exe files from this repo need to be places in C:\windows\ for windows users please see http://www.seleniumhq.org/ for install instructions for other OS's
+```
+python linkbot.py
+```
+the bot will take about 20 seconds to get logged in and ready to go then you will see
+```
+===============================LINKBOT ENGAGED!!!!========================================
+..........................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................
+```
+this means that it is running and each dot represents a sent connection request.
---
+++ linkbot.py
@@ -0,0 +1,58 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+import time, random, sys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+email = 'osteth@gmail.com'
+password = '1Pterodactyl!'
+
+print("OPENING CHROME")
+print("\n")
+driver = webdriver.Chrome()
+print("Navigating to Login Page")
+print('\n')
+driver.get("https://www.linkedin.com/uas/login?session_redirect=%2Fvoyager%2FloginRedirect%2Ehtml&fromSignIn=true&trk=uno-reg-join-sign-in")
+time.sleep(5)
+account_imput = driver.find_element_by_id("session_key-login")
+password_input = driver.find_element_by_id("session_password-login")
+print('===============LOGGING IN=================')
+print('\n')
+time.sleep(5)
+account_imput.send_keys(email)
+password_input.send_keys(password)
+driver.find_element_by_css_selector("#btn-primary").click()
+print('Navigation to Netowrk Page')
+print('\n')
+time.sleep(5)
+driver.get("https://www.linkedin.com/mynetwork/")
+time.sleep(5)
+print('===============================LINKBOT ENGAGED!!!!========================================')
+while 1 == 1:
+    button_number = str(random.randint(1, 100))
+    item = '.'
+    print(item, sep=' ', end='', flush=True)
+    try:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.find_element_by_css_selector("ul.mn-pymk-list__cards > li:nth-of-type(" + button_number + ") > div.mn-pymk-list__action-container > button.button-secondary-small > span:nth-of-type(1)").click()
+    except:
+        pass
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
